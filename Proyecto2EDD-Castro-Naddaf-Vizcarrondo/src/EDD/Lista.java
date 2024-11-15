db34592dc75c26ba71686abec9f2f8bc288a78c0
samanthacastrorajbe <samanthacastrorajbe@gmail.com>
2024-11-14 20:44:26 -0400

    lista y nodo

--- Proyecto2EDD-Castro-Naddaf-Vizcarrondo/src/EDD/Lista.java
+++ Proyecto2EDD-Castro-Naddaf-Vizcarrondo/src/EDD/Lista.java
@@ -10,4 +10,43 @@ package EDD;
  */
 public class Lista {
     
+    private Nodo pFirst;
+    private int size;
+
+    /**
+     * Constructor que inicializa la lista
+     */
+    public Lista() { //eliminamos los parametros
+        this.pFirst = null;
+        this.size = 0;
+    }
+//métodos getters y setters para los atributos pFirst y size
+    /**
+     * Obtiene el primer nodo de la lista.
+     * @return El primer nodo.
+     */
+    public Nodo getpFirst() {
+        return pFirst;
+    }
+/**
+ * Establece el nodo1 de la lista.
+ * @param pFirst El nuevo nodo1.
+ */
+    public void setpFirst(Nodo pFirst) {
+        this.pFirst = pFirst;
+    }
+/**
+ * Obtiene el tamaño de la lista
+ * @return numero de elementos dentro de la lista
+ */
+    public int getSize() {
+        return size;
+    }
+/**
+ * Define tamaño de la lista
+ * @param size nuevo tamaño de Lista.
+ */
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
