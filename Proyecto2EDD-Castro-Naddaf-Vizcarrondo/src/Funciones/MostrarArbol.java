ae2abde1d38469a1452e704d8b4127aa9a2425dc
samanthacastrorajbe <samanthacastrorajbe@gmail.com>
2024-11-24 09:45:01 -0400

    avance en mostrar arbol

--- Proyecto2EDD-Castro-Naddaf-Vizcarrondo/src/Funciones/MostrarArbol.java
+++ Proyecto2EDD-Castro-Naddaf-Vizcarrondo/src/Funciones/MostrarArbol.java
@@ -4,10 +4,33 @@
  */
 package Funciones;
 
+import EDD.Arbol;
+import org.graphstream.ui.swing_viewer.ViewPanel;
+import org.graphstream.ui.view.Viewer;
+
 /**
  *
  * @author samantha
  */
+
+
+/**
+ * Clase para mostrar un árbol genealógico utilizando GraphStream. Los nodos son
+ * representados como gráficos con estilo personalizado.
+ */
+
 public class MostrarArbol {
+    private Arbol arbol; // Estructura del árbol genealógico
+    private Viewer visor; // Visor de GraphStream
+    private ViewPanel panelVista; // Panel de visualización del grafo
     
+    /**
+     * Constructor que inicializa el visualizador con un árbol específico.
+     *
+     * @param arbol el árbol genealógico a visualizar
+     */
+    public MostrarArbol(Arbol arbol) {
+        this.arbol = arbol;
+        
+    }
 }
